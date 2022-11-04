0820873312baf54701b2b1fe215a63c0acd0e5ce
Aziz <aziztunc99@gmail.com>
2022-11-04 15:56:55 +0300

    added

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// var about=document.getElementById("about");
+// var contact=document.getElementById("contact");
+// var index=document.getElementById("index");
+// var project=document.getElementById("project");
+
+// about.onclick=function()
+// {
+//     location.replace("http://127.0.0.1:5500/html/about.html")
+// }
+ 
